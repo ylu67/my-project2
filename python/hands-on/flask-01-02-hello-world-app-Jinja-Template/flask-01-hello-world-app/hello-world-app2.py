7e4cd2ce9386a57bd2cb4c6cb1e0a9f992af4747
ylu67 <udunyalcin@gmail.com>
2021-10-12 20:07:56 -0700

    flask-01-02 added

---
+++ python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app2.py
@@ -0,0 +1,26 @@
+import re
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello World from Flask!!!"
+
+@app.route('/second')
+def second():
+    return 'Bize her yer turkey!!!'
+@app.route('/third/subthird')
+def third():
+    return 'This is the subpage of third page'
+
+
+@app.route('/forth/<string:id>')
+def forth(id):
+    return f'Id number of this page is {id}'
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
